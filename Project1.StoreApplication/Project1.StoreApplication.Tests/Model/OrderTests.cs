2699fd458d4a5f21f0339566604a36ad12a4cb57
KyleHillRevature <kyle.hill@revature.net>
2021-11-08 01:26:05 -0500

    started writing test for order model method. realized that the test won't mean anything

--- Project1.StoreApplication/Project1.StoreApplication.Tests/Model/OrderTests.cs
+++ Project1.StoreApplication/Project1.StoreApplication.Tests/Model/OrderTests.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Xunit;
 using Moq;
 using Project1.StoreApplication.Domain.InputModels;
+using Project1.StoreApplication.Domain.Models;
+using Project1.StoreApplication.Domain.Interfaces.Repository;
 
 namespace Project1.StoreApplication.Tests.Model
 {
@@ -13,14 +15,31 @@ namespace Project1.StoreApplication.Tests.Model
     {
         //OrderInput addItem = new OrderInput() { OrderId = Guid.NewGuid()};
         
-        public static IEnumerable<object[]> OrderInputs =
-        new List<object[]>
-        {
-            new object[] { "xUnit", 1 },
+        //public static IEnumerable<object[]> OrderInputs =
+        //new List<object[]>
+        //{
+        //    new object[] { "xUnit", 1 },
            
-        };
-        [Theory, MemberData(nameof(OrderInputs))]
-        public void update_allScenarios(string blah, int ha) 
-        { }
+        //};
+        //[Theory, MemberData(nameof(OrderInputs))]
+        [Fact]
+        public void addItem_itemIsAvailable_itemAddedToCart() 
+        {
+            //Arrange
+            var orderItemRepoStub = new Mock<IOrderItemRepository>();
+            var orderRepoStub = new Mock<IOrderRepository>();
+            Order order1 = new Order()
+            {
+                Id = Guid.NewGuid(), OrderDate = DateTime.Parse(Order.cartOrderDate),
+                
+
+                };
+            Order order = new Order();
+
+
+            //Act
+            //Assert
+
+}
     }
 }
